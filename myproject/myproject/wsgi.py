9c88c5b6b4c84753f7dcbc6c7b2bce1b5c9fd080
IWSD <namidano12@gmail.com>
2026-01-05 20:21:15 +0900

    Fix wsgi

--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -19,10 +19,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE",
-    "myproject.myproject.settings"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+# os.environ.setdefault(
+#     "DJANGO_SETTINGS_MODULE",
+#     "myproject.myproject.settings"
+# )
 
 application = get_wsgi_application()
 
