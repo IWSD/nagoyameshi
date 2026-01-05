0c6273d6eff463a0d6031303d092cff654f13ebf
IWSD <namidano12@gmail.com>
2026-01-05 15:28:22 +0900

    Fix DJANGO_SETTINGS_MODULE path in wsgi.py

--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -7,10 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+
+# application = get_wsgi_application()
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "myproject.myproject.settings"
+)
 
 application = get_wsgi_application()
+
